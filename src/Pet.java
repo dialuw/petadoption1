9c7bc4b26a2030b310ce3b1a6b7baf578e682566
user <armiyanova.di@gmail,com>
2025-12-17 18:34:59 +0500

    pet class

---
+++ src/Pet.java
@@ -0,0 +1,12 @@
+public class Pet {
+
+    private String name;
+    private int age;
+    private String kind;
+
+    public Pet(String name, int age, String kind) {
+        this.name = name;
+        this.age = age;
+        this.kind = kind;
+    }
+}
